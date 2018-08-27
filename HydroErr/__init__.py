a892f01823acfe4b1450e51cba682264176068cd
Wade Roberts <waderoberts123@gmail.com>
2018-08-27 14:54:48 -0600

    Deleted files that weren't needed

--- HydroErr/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-For full documentation, see https://waderoberts123.github.io/HydroErr/
-"""
-
-from HydroErr.HydroErr import *
--- README.md
+++
@@ -1,2 +0,0 @@
-# HydroErr
-Goodness of Fit metrics for use in comparison studies, specifically in the field of hydrology.
